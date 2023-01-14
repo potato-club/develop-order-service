d8e189c7992eaae19d594b012fdd0747b0d68369
JoGeumJu <ccho8702@naver.com>
2023-01-14 13:11:17 +0900

    [#2] Change Header - Hedaer 폴더 위치 변경 - Header 퍼블리싱 완료 - 기능 미구현

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import styled from "styled-components";
 import type { AppProps } from "next/app";
-import { Header } from "../components/header/Header";
+import { Header } from "../src/components/header/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
---
+++ src/components/header/HeaderMenu.tsx
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+import Image from "next/image";
+import { customColor } from "../customColor";
+import { TbWorld } from "react-icons/tb";
+import { MenuButtons } from "./components/MenuButtons";
+import { MyInfo } from "./components/MyInfo";
+
+export const HeaderMenu = () => {
+  return (
+    <Wrapper>
+      <WrapperInner>
+        <Menu>
+          <MainButton>
+            <TbWorld fontSize={20} />
+            DOS
+          </MainButton>
+          <MenuButtons />
+        </Menu>
+        <MyInfo />
+      </WrapperInner>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 40px;
+  justify-content: center;
+  align-items: center;
+  background: ${customColor.indigo2};
+`;
+const WrapperInner = styled.div`
+  display: flex;
+  flex-direction: row;
+  max-width: 1024px;
+  width: 100%;
+  height: 100%;
+  padding: 12px;
+  justify-content: space-between;
+  align-items: center;
+  color: ${customColor.white};
+`;
+const Menu = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0 68px;
+`;
+const MainButton = styled.div`
+  display: flex;
+  flex-direction: row;
+  font-size: 16px;
+  font-weight: bold;
+  color: ${customColor.white};
+  gap: 0 8px;
+  align-items: center;
+`;
