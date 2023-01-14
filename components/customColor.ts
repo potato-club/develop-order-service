d531dfdd1e3648b88567e285048289ae86f6b199
JoGeumJu <ccho8702@naver.com>
2023-01-14 10:30:33 +0900

    [#1] 환경설정 - customColor 추가 - styled-components, recoil, react-icons 설치

---
+++ components/customColor.ts
@@ -0,0 +1,28 @@
+export type customColorType = {
+  white: string;
+  black: string;
+  gray: string;
+  lightGray: string;
+  darkGray: string;
+  blue: string;
+  blue_: string;
+  yellow_: string;
+  indigo1: string;
+  indigo2: string;
+  indigo3: string;
+};
+
+export const customColor: customColorType = {
+  white: "#FFFFFF",
+  black: "#000000",
+  gray: "#999999",
+  lightGray: "#BCBCBC",
+  darkGray: "#ECECEC",
+
+  blue: "#91D8F7",
+  blue_: "#BBEFFD48",
+  yellow_: "#FFE56432",
+  indigo1: "#17181D",
+  indigo2: "#212329",
+  indigo3: "#292C33",
+};
