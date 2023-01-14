b8c249ffaf2d43c78e75182dc987928d53fc0059
JoGeumJu <ccho8702@naver.com>
2023-01-14 10:36:59 +0900

    [#2] Add Header Init - _app서 Header 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import styled from "styled-components";
+import type { AppProps } from "next/app";
+import { Header } from "../components/header/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Wrapper>
+      <Header />
+      <Component {...pageProps} />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
