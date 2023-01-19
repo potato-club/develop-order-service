0a2f9f1b7acab88ec37f97a999d0bfa0ea5fdd00
JoGeumJu <ccho8702@naver.com>
2023-01-20 04:50:08 +0900

    [#6] Add Footer Init - FooterCompany, FooterLogin 추가

---
+++ src/components/footer/FooterCompany.tsx
@@ -0,0 +1,80 @@
+import styled from "styled-components";
+import { customColor } from "../customColor";
+
+export const FooterCompany = () => {
+  return (
+    <Wrapper>
+      <WrapperInner>
+        <Content>
+          <CompanyName>DOS</CompanyName>
+          <CompanyInfo>
+            <Info>
+              DOS ㈜어쩌고 저쩌고 사업자등록 어쩌고 저쩌고 101-23-45667
+              통신판매신고 2023-01-12
+            </Info>
+            <CompanyInfo_>
+              <Info>
+                개인정보보호책임자 : 아무개 서울시 어디구 어디동 24, 21층
+              </Info>
+              <Division />
+              <Info>고객만족센터 1588-1111</Info>
+            </CompanyInfo_>
+            <CompanyInfo_>
+              <Info>Tel. 032-532-1234</Info>
+              <Division />
+              <Info>Email. ccho8702@naver.com</Info>
+            </CompanyInfo_>
+          </CompanyInfo>
+        </Content>
+      </WrapperInner>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+  background: ${customColor.indigo1};
+`;
+const WrapperInner = styled.div`
+  display: flex;
+  max-width: 1024px;
+  width: 100%;
+  padding: 40px 20px;
+  align-items: center;
+`;
+const Content = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 0 32px;
+`;
+const CompanyName = styled.span`
+  display: flex;
+  font-size: 14px;
+  font-weight: bold;
+  color: ${customColor.gray};
+`;
+const CompanyInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 10px;
+  color: ${customColor.gray};
+  gap: 6px 0;
+`;
+const CompanyInfo_ = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+const Info = styled.p`
+  display: flex;
+`;
+const Division = styled.div`
+  display: flex;
+  width: 1px;
+  height: 8px;
+  background: ${customColor.gray};
+  margin: 0 6px;
+`;
---
+++ src/components/footer/FooterLogin.tsx
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+import { customColor } from "../customColor";
+
+export const FooterLogin = () => {
+  return (
+    <Wrapper>
+      <WrapperInner>
+        <Content>
+          발주신청을 하고싶다면?<LoginButton>로그인하기</LoginButton>
+        </Content>
+      </WrapperInner>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 60px;
+  justify-content: center;
+  align-items: center;
+  background: ${customColor.indigo2};
+`;
+const WrapperInner = styled.div`
+  display: flex;
+  flex-direction: row;
+  max-width: 1024px;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+`;
+const Content = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 0 16px;
+  color: ${customColor.white};
+  font-size: 16px;
+  align-items: center;
+`;
+const LoginButton = styled.button`
+  display: flex;
+  width: 112px;
+  height: 38px;
+  font-size: 14px;
+  font-weight: bold;
+  color: ${customColor.black};
+  background: ${customColor.blue};
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
