b657e0d7b2124b677893b702c6799ba0b1c9f5ed
JoGeumJu <ccho8702@naver.com>
2023-01-19 21:23:16 +0900

    [#6] Add Footer Init - Footer 추가

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Footer = () => {
+  return <Wrapper>푸터</Wrapper>;
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
