eea0982d1b852f7d5a5371943f01ae40b5f41c64
JoGeumJu <ccho8702@naver.com>
2023-01-20 04:48:58 +0900

    [#6] Add Footer Init - _app에 Footer추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import "../styles/globals.css";
 import styled from "styled-components";
 import type { AppProps } from "next/app";
 import { Header } from "../src/components/header/Header";
+import { Footer } from "../src/components/footer/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Wrapper>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </Wrapper>
   );
 }
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,18 @@
 import styled from "styled-components";
+import { FooterCompany } from "./FooterCompany";
+import { FooterLogin } from "./FooterLogin";
 
 export const Footer = () => {
-  return <Wrapper>푸터</Wrapper>;
+  return (
+    <Wrapper>
+      <FooterLogin />
+      <FooterCompany />
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+  width: 100%;
 `;
