12b95d1c245ede3d9036b877a746679baa48fdb9
ppacman <smwfkim2@naver.com>
2023-02-26 12:07:26 +0900

    [#13 Feat InformationPage - 페이지 배치 수정

--- src/infoPage/InformationPage.tsx
+++ src/infoPage/InformationPage.tsx
@@ -1,11 +1,79 @@
-import React from 'react';
-import PersonInfo from './PersonInfo';
+import styled from "styled-components";
+import React from "react";
+import Hyoseong from "./Hyoseong";
+import Geumju from "./Geumju";
+import Cheongjo from "./Cheongjo";
+import Haeyeon from "./Haeyeon";
+import Junhyung from "./Junhyung";
+import Image from "next/image";
+import information from "../assets/img/information/information.png";
+import schedule from "../assets/img/information/schedule.png";
+import character from "../assets/img/information/character.png";
+
 const InformationPage = () => {
   return (
-    <div>
-      <PersonInfo />
-    </div>
+    <Wrapper>
+      <InfoHead>
+        <Image src={information} alt={""} width={50} />
+        <InfoHeadText>직원정보</InfoHeadText>
+      </InfoHead>
+      <StickWrapper>
+        <Hyoseong />
+
+        <Geumju />
+
+        <Cheongjo />
+
+        <Haeyeon />
+
+        <Junhyung />
+      </StickWrapper>
+      <ScheduleHead>
+        <Image src={schedule} alt={""} width={50} />
+        <ScheduleHeadText>스케쥴</ScheduleHeadText>
+      </ScheduleHead>
+      <ScheduleBox>
+        
+      
+      </ScheduleBox>
+
+      
+    </Wrapper>
   );
 };
 
 export default InformationPage;
+const Wrapper = styled.div``;
+const InfoHead = styled.div`
+  display: flex;
+  margin: 40px;
+`;
+const InfoHeadText = styled.p`
+  align-self: center;
+  font-size: 40px;
+  font-weight: bolder;
+  margin-left: 20px;
+`;
+const StickWrapper = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 150px;
+`;
+const ScheduleHead = styled.div`
+  display: flex;
+  margin: 40px;
+`;
+
+const ScheduleHeadText = styled.div`
+  align-self: center;
+  font-size: 40px;
+  font-weight: bolder;
+  margin-left: 20px;
+`;
+const ScheduleBox = styled.div`
+`;
+
+
+
+
