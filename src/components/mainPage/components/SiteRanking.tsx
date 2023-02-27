0323004f471e380dfb54e6e42a6ff2489b8ad426
JoGeumJu <ccho8702@naver.com>
2023-02-27 17:56:32 +0900

    [#3] Feat MainPage - scrollButton 추가 - swiper css 변경

--- src/components/mainPage/components/SiteRanking.tsx
+++ src/components/mainPage/components/SiteRanking.tsx
@@ -5,72 +5,113 @@ import SwiperCore, { Autoplay, EffectCoverflow } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
-import img3 from "../../../assets/img/dummy/3.jpeg";
-import img2 from "../../../assets/img/dummy/2.jpeg";
+import { SiteSlideInfo } from "./SiteSlideInfo";
 
 export const SiteRanking = () => {
   SwiperCore.use([EffectCoverflow, Autoplay]);
   return (
     <Wrapper>
-      <StyledSwiper
-        slidesPerView={"auto"}
-        loop={true}
-        spaceBetween={-380}
-        slideToClickedSlide
-        centeredSlides
-        // autoplay={{ delay: 5000, disableOnInteraction: false }}
-        modules={[EffectCoverflow]}
-        effect={"coverflow"}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 300,
-          modifier: 1.2,
-          slideShadows: false,
-        }}
-      >
-        <StyledSwiperSlide key={"1"}>
-          <SiteInfo>
-            <SiteImg>
-              <Image src={img2} alt="1" fill style={{ objectFit: "contain" }} />
-              <Rank>1위</Rank>
-            </SiteImg>
-            <InfoContent>응앤</InfoContent>
-          </SiteInfo>
-        </StyledSwiperSlide>
-        <StyledSwiperSlide key={"2"}>
-          <SiteInfo>
-            <SiteImg>
-              <Image src={img2} alt="2" fill style={{ objectFit: "contain" }} />
-              <Rank>2위</Rank>
-            </SiteImg>
-          </SiteInfo>
-        </StyledSwiperSlide>
-        <StyledSwiperSlide key={"3"}>
-          <SiteInfo>
-            <SiteImg>
-              <Image src={img2} alt="3" fill style={{ objectFit: "contain" }} />
-              <Rank>3위</Rank>
-            </SiteImg>
-          </SiteInfo>
-        </StyledSwiperSlide>
-        <StyledSwiperSlide key={"4"}>
-          <SiteInfo>
-            <SiteImg>
-              <Image src={img2} alt="4" fill style={{ objectFit: "contain" }} />
-              <Rank>4위</Rank>
-            </SiteImg>
-          </SiteInfo>
-        </StyledSwiperSlide>
-        <StyledSwiperSlide key={"5"}>
-          <SiteInfo>
-            <SiteImg>
-              <Image src={img2} alt="5" fill style={{ objectFit: "contain" }} />
-              <Rank>5위</Rank>
-            </SiteImg>
-          </SiteInfo>
-        </StyledSwiperSlide>
-      </StyledSwiper>
+      <WrapperInner>
+        <StyledSwiper
+          slidesPerView={"auto"}
+          loop={true}
+          spaceBetween={50}
+          slideToClickedSlide
+          centeredSlides
+          // autoplay={{ delay: 5000, disableOnInteraction: false }}
+          modules={[EffectCoverflow]}
+          effect={"coverflow"}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 0,
+            depth: 280,
+            modifier: 1.2,
+            slideShadows: false,
+          }}
+        >
+          <StyledSwiperSlide key={"1"}>
+            <SiteInfo>
+              <SiteImg>
+                <Image
+                  src={"/img/dummy/2.jpeg"}
+                  alt="1"
+                  fill
+                  style={{ objectFit: "cover", borderRadius: "inherit" }}
+                />
+              </SiteImg>
+              <SiteSlideInfo
+                rank="medal1"
+                like={29}
+                star={5}
+                title="마우스 클릭을 위한 사이트 'WWW'"
+                tag={["파란", "정적"]}
+              />
+            </SiteInfo>
+          </StyledSwiperSlide>
+          <StyledSwiperSlide key={"2"}>
+            <SiteInfo>
+              <SiteImg>
+                <Image
+                  src={"/img/dummy/2.jpeg"}
+                  alt="2"
+                  fill
+                  style={{ objectFit: "cover", borderRadius: "inherit" }}
+                />
+              </SiteImg>
+              <SiteSlideInfo
+                rank="medal2"
+                like={29}
+                star={5}
+                title="마우스 www"
+                tag={["파란", "정적"]}
+              />
+            </SiteInfo>
+          </StyledSwiperSlide>
+          <StyledSwiperSlide key={"3"}>
+            <SiteInfo>
+              <SiteImg>
+                <Image
+                  src={"/img/dummy/2.jpeg"}
+                  alt="3"
+                  fill
+                  style={{ objectFit: "cover", borderRadius: "inherit" }}
+                />
+              </SiteImg>
+              <SiteSlideInfo
+                rank="medal3"
+                like={29}
+                star={5}
+                title="마우스 www"
+                tag={["파란", "정적"]}
+              />
+            </SiteInfo>
+          </StyledSwiperSlide>
+          <StyledSwiperSlide key={"4"}>
+            <SiteInfo>
+              <SiteImg>
+                <Image
+                  src={"/img/dummy/2.jpeg"}
+                  alt="4"
+                  fill
+                  style={{ objectFit: "cover", borderRadius: "inherit" }}
+                />
+              </SiteImg>
+            </SiteInfo>
+          </StyledSwiperSlide>
+          <StyledSwiperSlide key={"5"}>
+            <SiteInfo>
+              <SiteImg>
+                <Image
+                  src={"/img/dummy/2.jpeg"}
+                  alt="5"
+                  fill
+                  style={{ objectFit: "cover", borderRadius: "inherit" }}
+                />
+              </SiteImg>
+            </SiteInfo>
+          </StyledSwiperSlide>
+        </StyledSwiper>
+      </WrapperInner>
     </Wrapper>
   );
 };
@@ -79,12 +120,19 @@ const Wrapper = styled.div`
   display: flex;
   height: 100%;
   width: 100%;
+  justify-content: center;
+`;
+const WrapperInner = styled.div`
+  display: flex;
+  width: 100%;
+  height: auto;
   overflow: hidden;
 `;
 const StyledSwiper = styled(Swiper)`
   display: flex;
   width: 100%;
-  height: 100%;
+  max-width: 430px;
+  overflow: visible;
   .swiper-wrapper {
     display: flex;
     flex-direction: row;
@@ -97,18 +145,23 @@ const StyledSwiperSlide = styled(SwiperSlide)`
   align-items: center;
   width: 100%;
   height: 100%;
-  padding: 40px;
-  padding-top: 60px;
+  padding: 14px 0;
 `;
-const SiteInfo = styled.div`
+const SiteInfo = styled.button`
   display: flex;
+  position: relative;
   flex-direction: column;
-  background: ${customColor.blue};
+  background: ${customColor.white};
   width: 608px;
   height: 100%;
-  box-shadow: 0px 2px 4px 0px ${customColor.black + "33"};
+  max-height: 330px;
+  box-shadow: 0px 2px 4px 2px ${customColor.black + "33"};
   align-items: center;
   justify-content: flex-end;
+  padding: 28px 16px;
+  margin-top: 8px;
+  border-radius: 6px;
+  cursor: pointer;
 `;
 const SiteImg = styled.div`
   display: flex;
@@ -118,25 +171,7 @@ const SiteImg = styled.div`
   width: 100%;
   max-width: 580px;
   max-height: 460px;
-  transform: translateY(-50px);
-`;
-const Rank = styled.div`
-  display: flex;
-  position: absolute;
-  background: blue;
-  width: max-content;
-  height: max-content;
-  color: white;
-  font-size: 20px;
-  padding: 8px 10px;
-  top: 0px;
-  left: 8px;
-`;
-const InfoContent = styled.div`
-  display: flex;
-  position: absolute;
-  width: 608px;
-  height: 58px;
-  padding: 20px;
-  align-items: center;
+  transform: translateY(-44px);
+  box-shadow: 0px 2px 4px 3px ${customColor.black + "33"};
+  border-radius: 6px;
 `;
