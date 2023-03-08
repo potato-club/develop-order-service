e5f3e92de28983f3ab9ae2f61377fef94d92b173
Joonhyung Choi <joonhyungaaa@gmail.com>
2023-03-09 02:04:23 +0900

    ReviewBottom 컴포넌트 수정
    ReviewBottom 컴포넌트 수정

--- src/components/orderReviewPage/ReviewBottom.tsx
+++ src/components/orderReviewPage/ReviewBottom.tsx
@@ -1,10 +1,44 @@
 import styled from "styled-components";
-import { Pagenation } from "./Pagenation";
+import Image from "next/image";
+import arrowIcon from "../../../public/img/review/arrow.png";
+import { useState } from "react";
 
 export const ReviewBottm = () => {
+  const [pageNumState, setPageNumState] = useState(1);
+  const prevPageButton = () => {
+    setPageNumState(pageNumState - 1);
+  };
+
+  const nextPageButton = () => {
+    setPageNumState(pageNumState + 1);
+  };
   return (
     <BottomWrapper>
-      <Pagenation />
+      <PagenationDiv>
+        <ArrowImgDiv arrow={"left"} pageNumState={pageNumState}>
+          <ArrowA onClick={prevPageButton}>
+            <Image
+              src={arrowIcon}
+              alt="arrowIcon"
+              width={36}
+              height={36}
+            ></Image>
+          </ArrowA>
+        </ArrowImgDiv>
+        <PageNumberDiv>
+          <PageNumberP>{pageNumState}</PageNumberP>
+        </PageNumberDiv>
+        <ArrowImgDiv arrow={"right"} pageNumState={pageNumState}>
+          <ArrowA onClick={nextPageButton}>
+            <Image
+              src={arrowIcon}
+              alt="arrowIcon"
+              width={36}
+              height={36}
+            ></Image>
+          </ArrowA>
+        </ArrowImgDiv>
+      </PagenationDiv>
     </BottomWrapper>
   );
 };
@@ -14,4 +48,37 @@ const BottomWrapper = styled.div`
   width: 100%;
   display: flex;
   align-items: center;
+`;
+
+const PagenationDiv = styled.div`
+  height: 36px;
+  width: 155px;
+  margin: 0 auto;
+`;
+
+const ArrowImgDiv = styled.div<{ arrow: String; pageNumState: Number }>`
+  width: 36px;
+  height: 100%;
+  float: left;
+  transform: ${(props) => (props.arrow === "right" ? "rotate(180deg)" : "")};
+  cursor: pointer;
+  visibility: ${(props) =>
+    props.arrow === "left" && props.pageNumState === 1 ? "hidden" : ""};
+`;
+
+const ArrowA = styled.a``;
+
+const PageNumberDiv = styled.div`
+  width: 83px;
+  height: 100%;
+  float: left;
+`;
+
+const PageNumberP = styled.p`
+  display: block;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  font-size: 30px;
+  line-height: 36px;
 `;
