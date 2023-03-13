b10283f2e0c4aefcf7cd5078ac77cad31e3eeb27
JoGeumJu <ccho8702@naver.com>
2023-03-13 15:52:08 +0900

    [#3] Feat MainPage - obserber 애니메이션 및 퍼블리싱 완료

--- src/components/mainPage/components/Publishing.tsx
+++ src/components/mainPage/components/Publishing.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { customColor } from "../../customColor";
 import { RiDoubleQuotesL, RiDoubleQuotesR } from "react-icons/ri";
 import { ImBook } from "react-icons/im";
+import Image from "next/image";
 
 export const Publishing = () => {
   const { ref, inView, entry } = useInView({
@@ -18,17 +19,128 @@ export const Publishing = () => {
           <DoubleQuotesR />
         </Explain>
       </Title>
-      <WrapperInner>
-        <Header>
-          <ImBook color={customColor.blue} />
-          2022-12-12
+      <WrapperInner className={inView ? "publishing" : "publishing1"}>
+        <Header className={inView ? "publishing" : "publishing1"}>
+          <ImBook color={customColor.blue} size={16} />
+          XXXX-XX-XX
         </Header>
         <Body>
-          <BodyLeft>
-            <BodyInner></BodyInner>
+          <BodyLeft className={inView ? "publishing" : "publishing2"}>
+            <BodyInner>
+              <Image
+                src="/img/main/stickers/sticker10.png"
+                alt="sticker"
+                width={400}
+                height={420}
+                style={{
+                  position: "absolute",
+                  bottom: "-40px",
+                  left: "-40px",
+                  transform: " rotate(-30deg)",
+                }}
+              />
+              <Image
+                src="/img/main/stickers/sticker7.png"
+                alt="sticker"
+                width={80}
+                height={100}
+                style={{ position: "absolute", bottom: "160px", left: "100px" }}
+              />
+              <Image
+                src="/img/main/stickers/sticker3.png"
+                alt="sticker"
+                width={100}
+                height={100}
+                style={{ position: "absolute", bottom: "150px", left: "30px" }}
+              />
+              <Image
+                src="/img/main/stickers/sticker5.png"
+                alt="sticker"
+                width={160}
+                height={180}
+                style={{ position: "absolute", bottom: "10px", left: "40px" }}
+              />
+              <Image
+                src="/img/main/stickers/sticker6.png"
+                alt="sticker"
+                width={180}
+                height={310}
+                style={{ position: "absolute", bottom: 0, right: 0 }}
+              />
+              <Image
+                src="/img/main/stickers/sticker8.png"
+                alt="sticker"
+                width={160}
+                height={130}
+                style={{
+                  position: "absolute",
+                  top: "50%",
+                  right: "-16px",
+                  transform: "translate(0,calc(-50% - 90px)) rotate(20deg)",
+                }}
+              />
+              <Date>XXXX년 XX월 XX일</Date>
+            </BodyInner>
           </BodyLeft>
-          <BodyRight>
-            <BodyInner></BodyInner>
+          <BodyRight className={inView ? "publishing" : "publishing2"}>
+            <BodyInner>
+              <Image
+                src="/img/main/stickers/sticker11.png"
+                alt="sticker"
+                fill
+                style={{
+                  objectFit: "cover",
+                  transform: "rotate(-180deg) translate(-100px,-100px)",
+                }}
+              />
+              <Image
+                src="/img/main/stickers/sticker4.png"
+                alt="sticker"
+                width={160}
+                height={160}
+                style={{
+                  position: "absolute",
+                  bottom: "20px",
+                  left: "20px",
+                }}
+              />
+              <Image
+                src="/img/main/stickers/sticker2.png"
+                alt="sticker"
+                width={100}
+                height={110}
+                style={{
+                  position: "absolute",
+                  bottom: "20px",
+                  left: "140px",
+                }}
+              />
+              <Image
+                src="/img/main/stickers/sticker1.png"
+                alt="sticker"
+                width={220}
+                height={280}
+                style={{
+                  position: "absolute",
+                  top: "50%",
+                  left: "10px",
+                  transform: "translate(0,calc(-50% - 60px))",
+                }}
+              />
+              <Image
+                src="/img/main/stickers/sticker9.png"
+                alt="sticker"
+                width={140}
+                height={120}
+                style={{
+                  position: "absolute",
+                  top: "50%",
+                  right: "20px",
+                  transform: "rotateY(-180deg) translate(0,-50%)",
+                }}
+              />
+              <Date style={{ right: 20 }}>XXXX년 XX월 XX일</Date>
+            </BodyInner>
           </BodyRight>
         </Body>
       </WrapperInner>
@@ -52,11 +164,7 @@ const Wrapper = styled.section`
   }
   .publishing2 {
     opacity: 0;
-    transform: translate(-20px, 12px);
-  }
-  .publishing2_ {
-    opacity: 0;
-    transform: translate(20px, 12px);
+    transform: translate(0, 12px);
   }
 `;
 const Title = styled.div`
@@ -106,6 +214,7 @@ const WrapperInner = styled.div`
   aspect-ratio: 16/9;
   background: #eee;
   overflow: hidden;
+  transition: all 0.6s ease 0.6s;
 `;
 const Header = styled.div`
   display: flex;
@@ -125,6 +234,7 @@ const Header = styled.div`
   font-size: 14px;
   gap: 6px;
   z-index: 1;
+  transition: all 0.6s ease 0.9s;
 `;
 const Body = styled.div`
   display: flex;
@@ -143,6 +253,7 @@ const BodyLeft = styled.div`
   background: #eee;
   padding: 16px;
   padding-right: 10px;
+  transition: all 0.6s ease 1.2s;
 `;
 const BodyRight = styled.div`
   display: flex;
@@ -151,12 +262,24 @@ const BodyRight = styled.div`
   background: #eee;
   padding: 16px;
   padding-left: 10px;
+  transition: all 0.6s ease 1.4s;
 `;
 const BodyInner = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
   background: #eee;
   width: 100%;
   height: 100%;
   box-shadow: -2px 4px 8px 2px ${customColor.black + "33"},
     2px -4px 8px 2px ${customColor.white};
   border-radius: 16px;
+  overflow: hidden;
+  padding: 24px 20px;
+`;
+const Date = styled.div`
+  display: flex;
+  position: absolute;
+  font-size: 16px;
+  font-weight: bold;
 `;
