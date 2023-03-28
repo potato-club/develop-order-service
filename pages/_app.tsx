f5a9339cf5fece571a507c06ef45ae2909d02249
JoGeumJu <ccho8702@naver.com>
2023-03-28 16:02:53 +0900

    [#28] Feat LoginPage - 로그인컨트롤 컴포넌트에 맞춰 스크롤 css 제어

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,14 @@ import { Header } from "../src/components/header/Header";
 import { Footer } from "../src/components/footer/Footer";
 import { Layout } from "../src/components/layout/Layout";
 import { RecoilRoot } from "recoil";
+import { ScrollCSS } from "../src/components/layout/ScrollCSS";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <Wrapper>
         <GlobalStyle />
+        <ScrollCSS />
         <Header />
         <Layout>
           <Component {...pageProps} />
