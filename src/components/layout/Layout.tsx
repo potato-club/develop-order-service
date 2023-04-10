8a4a4e6233847ed9cdc387e9171c127afb28e220
JoGeumJu <ccho8702@naver.com>
2023-04-10 18:59:54 +0900

    [#36] Feat AdminPage - Admin 관련 페이지일시, Layout 변경

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,24 +1,39 @@
 import styled from "styled-components";
+import Router from "next/router";
 import { GlobalStyle } from "../../../styles/global.style";
 import { customColor } from "../customColor";
 import { Footer } from "../footer/Footer";
 import { Header } from "../header/Header";
 import { ScrollCSS } from "./ScrollCSS";
+import { useEffect, useState } from "react";
 
 interface Props {
   children: React.ReactNode;
 }
 
 export const Layout = ({ children }: Props) => {
+  const [isAdmin, setIsAdmin] = useState(false);
+  useEffect(() => {
+    setIsAdmin(Router.asPath.includes("/admin"));
+  }, []);
+
   return (
     <Wrapper>
-      <GlobalStyle />
-      <ScrollCSS />
-      <Header />
-      <LayoutWrapper>
-        <LayoutWrapperInner>{children}</LayoutWrapperInner>
-      </LayoutWrapper>
-      <Footer />
+      {isAdmin ? (
+        <>
+          <GlobalStyle />
+          {children}
+        </>
+      ) : (
+        <>
+          <ScrollCSS />
+          <Header />
+          <LayoutWrapper>
+            <LayoutWrapperInner>{children}</LayoutWrapperInner>
+          </LayoutWrapper>
+          <Footer />
+        </>
+      )}
     </Wrapper>
   );
 };
