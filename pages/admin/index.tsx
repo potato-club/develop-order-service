f9aa466a4a1cc9c8242a60742d4917c6f469bc6e
JoGeumJu <ccho8702@naver.com>
2023-04-10 17:08:21 +0900

    [#36] Feat AdminPage - 기초 컨테이너 생성

---
+++ pages/admin/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Admin: NextPage = () => {
+  return <></>;
+};
+
+export default Admin;
---
+++ src/components/adminPage/AdminPage.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const AdminPage = () => {
+  return <></>;
+};
+
+const Wrapper = styled.section``;
