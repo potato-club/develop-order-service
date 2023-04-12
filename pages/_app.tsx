83715a9c0af66f310a4956918f94ff2c28b988f3
ppacman <smwfkim2@naver.com>
2023-04-12 19:30:36 +0900

    [#13] Add informationPage - 로딩기능 구현

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,39 @@ import { Header } from "../src/components/header/Header";
 import { Footer } from "../src/components/footer/Footer";
 import { Layout } from "../src/components/layout/Layout";
 import { RecoilRoot } from "recoil";
+import { useEffect, useState } from "react";
+import Router from "next/router";
+import LoadingSpinner from "../src/components/loading/LoadingSpinner";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const showRoute = ["/", "/login", "/signUp", "/Information"];
+    const start = (url: string, { shallow }: { shallow: boolean }) => {
+      if (showRoute.find((route) => String(url).includes(route))) {
+        setLoading(true);
+      }
+    };
+    const end = (url: string) => {
+      if (showRoute.find((route) => String(url).includes(route))) {
+        setLoading(false);
+      }
+    };
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
+
   return (
+    
     <RecoilRoot>
+       {loading && <LoadingSpinner fixed />}
       <Wrapper>
         <GlobalStyle />
         <Header />
