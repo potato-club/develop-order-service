837d903e9665e3b2e8b379bb1e23976105ecd548
ppacman <smwfkim2@naver.com>
2023-07-02 16:32:30 +0900

    [#50] Add ScheuduleModifPage - 스케쥴 등록용 함수 추가

--- src/apis/controller/scheduler.api.ts
+++ src/apis/controller/scheduler.api.ts
@@ -1,8 +1,13 @@
+import { ScheduleType } from "../../hooks/query/scheduler/useQueryGetSchedules";
 import sendApi from "../sendApi";
 
 export const SchedulerApi = {
   loadSchedules: async () => {
     const response = await sendApi.get("/admin/schedule");
     return response.data;
+  },
+  createSchedule: async (scheduleData: ScheduleType) => {
+    const response = await sendApi.post("/admin/schedule/register", scheduleData);
+    return response.data;
   }
-};
+}
