eb5eb410e7947d1c320d42967ae73b815d5d5fd9
ppacman <smwfkim2@naver.com>
2023-07-02 16:31:56 +0900

    [#50] Feat ScheduleModifyPage - API테스트용 코드 삭제

--- src/hooks/query/scheduler/useQueryGetSchedules.ts
+++ src/hooks/query/scheduler/useQueryGetSchedules.ts
@@ -22,8 +22,6 @@ export const useQueryGetSchedules = () => {
         color: item.color,
       }));
 
-      console.log("Swagger 데이터 확인:", schedulesRes); // Swagger 데이터를 확인하기 위한 로그
-
       return schedulesRes;
     } catch (error) {
       throw new Error("Failed to load schedules");
