fc471194588365c055f3f62c2d8fac64575e9ea4
ppacman <smwfkim2@naver.com>
2023-07-28 19:01:59 +0900

    [#50] Add ScheduleModifyPage - interface 파일 생성

---
+++ src/apis/controller/scheduler.api.type.ts
@@ -0,0 +1,18 @@
+export interface ScheduleType {
+    id : number,
+    name: string;
+    start: string;
+    end: string;
+    title: string;
+    color: string;
+  }
+
+
+  export interface SchedulePostType {
+    name: string;
+    start: string;
+    end: string;
+    title: string;
+    color: string;
+  }
+  
