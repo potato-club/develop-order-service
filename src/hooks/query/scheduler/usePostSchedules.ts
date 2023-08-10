4d11036656d84fd545ee70404bf7309ebfda8703
ppacman <smwfkim2@naver.com>
2023-08-10 13:50:07 +0900

    [#50] chore ScheduleModifyPage - 에러 메세지 수정

--- src/hooks/query/scheduler/usePostSchedules.ts
+++ src/hooks/query/scheduler/usePostSchedules.ts
@@ -1,5 +1,5 @@
 import { SchedulerApi } from "../../../apis/controller/scheduler.api";
-import { ScheduleType } from "./useGetSchedules";
+import { ScheduleType } from "../../../apis/controller/scheduler.api.type";
 
 export const createSchedule = async (schedule: ScheduleType) => {
     try {
@@ -9,7 +9,7 @@ export const createSchedule = async (schedule: ScheduleType) => {
       // 필요하다면 생성된 일정 데이터를 반환할 수 있습니다
       return response;
     } catch (error) {
-      console.error("일정 생성에 실패하였습니다:", error);
+      console.error("일정 생성에 실패하였습니다:", error);  
       // 필요한 대로 오류 처리
       throw new Error("일정 생성에 실패하였습니다");
     }
